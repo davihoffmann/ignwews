a7ce76e6668e1717569baeff3251209479834a4d
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-17 20:21:44 -0300

    - Desenvolvimento de componente ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import SignInButton from '../SignInButton';
 import styles from './styles.module.scss';
+import ActiveLink from '../ActiveLink';
 
 export function Header() {
   return (
@@ -9,12 +9,12 @@ export function Header() {
       <div className={styles.headerContent}>
         <Image src="/images/logo.svg" alt="ig.news" width="100" height="25" />
         <nav>
-          <Link href="/">
-            <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts">
+          <ActiveLink href="/" activeClassName={styles.active}>
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}>
             <a>Posts</a>
-          </Link>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
