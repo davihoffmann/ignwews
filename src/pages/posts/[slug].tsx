00dc5dcabce6167dff0212955be197761151c8cd
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2022-01-18 21:11:42 -0300

    - Desenvolvimento de verificaçao se usuário ja tem uma subscription ativa

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -38,16 +38,21 @@ export default function Post({ post }: PostProps) {
 }
 
 export const getServerSideProps: GetServerSideProps = async({ req, params }) => {
-  const session = getSession({req});
+  const session = await getSession({req});
   const { slug } = params;
 
-  // if(!session) {  
-  // }
+  if(!session.activeSubscription) {  
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID<any>('post', String(slug), {});
-
 
   const post = {
     slug,
